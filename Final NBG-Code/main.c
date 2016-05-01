80df7688e765d15ff820b551a6d92f5290579fe4
CMD <mac@jungkeeui-macbook-pro.local>
2016-05-01 10:52:02 +0900

    Initial Commit

---
+++ Final NBG-Code/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Final NBG-Code
+//
+//  Created by CMD on 2016. 5. 1..
+//  Copyright © 2016년 CMD. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
